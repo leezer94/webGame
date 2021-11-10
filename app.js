e2422652973379a02fe3a81d5ec4f880dc8143f9
leezer94 <2kunhee94@gmail.com>
2021-11-10 20:34:51 +0900

    feat: 끝말잇기 완성

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-// // prompt 는 String 으로 출력하기 때문에 숫자값 받을시  parseInt ( 10 진법 )
-// console.log(`number : ${number}`)
-// alert(number)
-// const yesOrNo = confirm('맞나요?')
 
 //Selector
 const number = parseInt(prompt('몇 명이 참가하나요 ?'),10)
@@ -26,11 +22,11 @@ $button.addEventListener('click', () => {
         }else {
             $order.textContent = order + 1
         }
-
+        $input.value=''
 
     }else {
-        if(newWord[0] === word[-1] ){
-            word === newWord;
+        if(word[word.length -1] === newWord[0] ){
+            word = newWord;
             $word.textContent = word;
             $input.value = '';
             const order = Number($order.textContent); // 현재 순서
@@ -40,6 +36,7 @@ $button.addEventListener('click', () => {
                 $order.textContent = order + 1
             }
         }else {
+            alert('단어가 올바르지 않습니다.')
 
         }
     }
