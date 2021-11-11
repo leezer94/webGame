5f486dafdbeabf11c66b9ddaf7f4efa78c2419a5
leezer94 <2kunhee94@gmail.com>
2021-11-11 16:27:21 +0900

    feat: valueChecking 기능 추가

--- baseball game/index.js
+++ baseball game/index.js
@@ -5,6 +5,8 @@ const logs = document.querySelector('.logs')
 
 const numbers = [];     //[1,2,3,4,5,6,7,8,9]
 const result = [];
+const tries = [];
+
 //Function
 for(let i=0; i < 9; i++){
     numbers.push(i + 1)
@@ -17,7 +19,32 @@ for(let i=0; i < 4; i++){
 }
 console.log(result)
 
+function checkInput(input){     // 검사하는 코드 (따로 분리해주는 것이 가독성이 좋음)
+    if(input.length !== 4){
+        return alert('4자리 숫자를 입력해 주세요.')
+    }
+    if(new Set(input).size !== 4) {
+        return alert('중복된 숫자가 없는지 확인해 주세요.')
+    }
+    if(tries.includes(input)) {
+        return alert('이미 시도했던 값 입니다.')
+    }
+    return true;
+}
 //Event
+form.addEventListener('submit', (e) => {
+    e.preventDefault() // 기본동작 prevent(refresh)
+    const value = input.value;  //  입력값 value 변수에 저장해준다.  same as e.target[0].value;
+    input.value = '';            // 지워준다
+    const valid = checkInput(value);    // 함수로 value 를 보내준다.
+    if(valid) {
+        tries.push(value);
+        console.log(tries)
+    }else {
+
+    }
+
+})
 
 
-// console.log(Math.floor(Math.random()*9+1))
+
